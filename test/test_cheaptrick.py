7e1959ffda48a65e260890f9bacb4c701b5dcd30
sabonerune <102559104+sabonerune@users.noreply.github.com>
2024-10-01 23:19:09 +0900

    FIX: fix import

--- test/test_cheaptrick.py
+++ test/test_cheaptrick.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 import pytest
 
 import wwopy
 
 if TYPE_CHECKING:
-    import numpy as np
     import numpy.typing as npt
 
 
