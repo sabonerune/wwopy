b126f4b02170da34489424ae245ecce3f0e4864f
sabonerune <102559104+sabonerune@users.noreply.github.com>
2024-11-15 18:10:52 +0900

    ENH: Generate licenses only if necessary. (#23)
    * ENH: Generate licenses only if necessary.
    
    Get the license file path for nanobind.
    Add it to your dependencies so changes can be detected.
    
    * FIX: cmake-format CI
    
    Show diff.
    
    * FIX: format  `CMakeLists.txt`

--- cmake/generate_license.py
+++ cmake/generate_license.py
@@ -1,49 +1,44 @@
 from __future__ import annotations
 
 import argparse
-import importlib.metadata
+from collections.abc import Iterable
 from pathlib import Path
 
 _project_root = Path(__file__).parents[1]
-
-
-def read_nanobind_license() -> str:
-    dist = importlib.metadata.distribution("nanobind")
-    version = dist.version
-    # NOTE: Use packaging when requirements increase
-    filename = "LICENSE" if version == "2.0.0" else "licenses/LICENSE"
-    text = dist.read_text(filename)
-    if text is None:
-        msg = "nanobind license file is not found."
-        raise Exception(msg)
-    return text
 
 
 def read_license_from_file(filepath: Path) -> str:
     return filepath.read_text("utf-8")
 
 
-def write_license(dist: Path, licenses: list[tuple[str, str]]):
+def write_license(dist: Path, licenses: Iterable[tuple[str, str]]):
     delimiter = "\n" + "=" * 80 + "\n\n"
     with dist.open(mode="wt", encoding="utf-8", newline="\n") as f:
-        f.write(read_license_from_file(_project_root / "LICENSE.txt") + "\n\n")
-        f.write("ThirdPartyLicenses")
-        for name, text in licenses:
+        project_license = (_project_root / "LICENSE.txt").read_text("utf-8")
+        f.write(project_license)
+        f.write("\n\nThirdPartyLicenses")
+        for name, license_path in licenses:
             f.write(delimiter)
             f.write(f"{name}\n")
             f.write("-" * len(name) + "\n\n")
-            f.write(text)
+            try:
+                license_text = Path(license_path).read_text("utf-8")
+            except OSError as e:
+                msg = f"Licene Error: {name}"
+                raise Exception(msg) from e
+            f.write(license_text)
 
 
 def main(args: list[str] | None = None):
     parser = argparse.ArgumentParser()
+    parser.add_argument("files", type=str, nargs="+")
     parser.add_argument("--dist", type=Path, required=True)
     parsed_arg = parser.parse_args(args)
-    licenses = [
-        ("WORLD", read_license_from_file(_project_root / "ext/World/LICENSE.txt")),
-        ("nanobind", read_nanobind_license()),
-    ]
-    write_license(parsed_arg.dist, licenses)
+    files: list[str] = parsed_arg.files
+    if len(files) % 2 == 1:
+        msg = "argment is wrong"
+        raise Exception(msg)
+    write_license(parsed_arg.dist, zip(files[0::2], files[1::2]))
 
 
 if __name__ == "__main__":
